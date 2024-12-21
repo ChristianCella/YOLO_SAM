908dfbcac2f979507492d8ff0544ad4aea81cab3
ChristianCella <christian.cella@polimi.it>
2024-12-21 17:50:08 +0100

    The codes are now ready

--- Training_pipeline/Inference.py
+++ Training_pipeline/Inference.py
@@ -1,10 +1,20 @@
 ''' 
 Sixth script: test the fine-tuned model.
+
+NOTE: when running the script, if you want to keep the LaTeX font in the plots,
+install all the packages suggested by the pop-up panels that show up when running the script.
 '''
 
 from ultralytics import YOLO
 from pathlib import Path
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import cv2
+
+# Imports for latex plots
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
 
 # Import the colors
 import sys
@@ -12,12 +22,12 @@ sys.path.append(".")
 import Utils.fonts as fonts
 
 # Object under exam
-object = "Bottles"
-label = "bottle"
+object = "Beckers"
+label = "becker"
 
 model = YOLO(Path("Model")/object/"train/weights/best.pt")
-image_path = Path("Images/Bottles/Validation_set/color_20241213_190319.png")
-save_directory = Path("Images")/object/"Inference_results"
+image_path = Path("Images/Random_validation_images/20241126_155938.jpg")
+save_directory = Path("Images")/object/"Inference"
 save_directory.mkdir(parents=True, exist_ok=True)
 
 # Inference
@@ -28,14 +38,15 @@ results = model.predict(
     save = False) 
 
 # Result of the detection
-detected_image = results[0].plot()
+detected_image = cv2.cvtColor(results[0].plot(), cv2.COLOR_BGR2RGB)
 
 # Save the image
 save_path = save_directory / f"{image_path.stem}_predictions.jpg"
 plt.imsave(str(save_path), detected_image)
 
 # Display the image
-plt.imshow(detected_image)
-plt.axis('off')  # Turn off axis for better visualization
-plt.title(f"Detected {label}")
+plt.figure(figsize=(8, 6))
+plt.imshow(detected_image, cmap = 'viridis')
+plt.axis('off')
+plt.title(rf"\textbf{{Detected {label}}}", fontsize=16)
 plt.show()
